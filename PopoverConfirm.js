46e77cce11cb04faf11e1826672cc4b6ed1b1fa7
Jean-Marc <jeanmarc.montout@outlook.fr>
2020-06-26 11:30:04 +0200

    Ajoute des méthodes mais ne peut plus appeler close()

--- PopoverConfirm.js
+++ PopoverConfirm.js
@@ -1,19 +1,24 @@
-class PopOverConfirm {
+class PopoverConfirm {
     constructor(datas) {
         this.trunk = datas.trunk;
         this.message = this.setMessage(datas);
+        // this.display();
 
-        this.display();
+        this.bindings();
+
+        return this;
+    }
+
+    bindings() {
+        $(document).on("click", ".popover-confirm-cancel", this.cancelPopover);
     }
 
     setMessage(datas) {
-        var msg = [
-            "<form class='confirm'>",
-        ];
+        var msg = ["<form class='confirm'>"];
         msg.push("<label>" + datas.msg + "</label>");
-        msg.push(
-            "<a href='#' class='button btn btn-xs btn-link popover-confirm-exit'>Annuler</a>"
-        );
+        // msg.push(
+        //     "<a href='#' class='button btn btn-xs btn-link popover-confirm-exit'>Annuler</a>"
+        // );
         msg.push(
             "<a href='#' class='button btn btn-xs btn-success popover-confirm-valid' >Oui</a>"
         );
@@ -25,33 +30,33 @@ class PopOverConfirm {
         return msg.join("");
     }
 
-
     display() {
-        
         // prepare popover
         this.trunk.popover({
             animation: false,
             delay: 0,
             html: true,
-            placement: 'left',
+            placement: "left",
             // viewport: ".commande-group",
-            content: this.message
+            content: this.message,
         });
 
         // active le popover
         this.trunk.popover("show");
     }
 
-    popConfirmAction(datas_checked) {
-        /**
-         * Affiche le popup de confirmation
-         * @type @exp;$@call;extend
-         */
+    cancelPopover(e) {
+        e.preventDefault();
+        console.log(PopoverConfirm);
+        // PopoverConfirm.close();
 
-        var datas = $.extend({}, datas_checked);
-        delete datas.input;
+        // this.trunk.on('hidden.bs.popover', function(){
+        //     alert('The popover is now hidden.');
+        //   });
+    }
 
-        console.log("datas_check pop");
-        console.log(datas);
+    close() {
+        alert("sdfsdf");
+        // this.trunk.popover("destroy");
     }
 }
