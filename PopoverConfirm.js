c298a8cba4c6397f29bf890d74d8a9cb8bd28d11
Jean-Marc <jeanmarc.montout@outlook.fr>
2020-06-26 09:57:37 +0200

    Clean up code

--- PopoverConfirm.js
+++ PopoverConfirm.js
@@ -1,26 +1,28 @@
 class PopOverConfirm {
-
     constructor(datas) {
-
         this.datas = datas;
-
-        // this.setMessage(datas);
-
-    };
-
+    }
 
     setMessage() {
-
-        var msg = ["<form class='confirm' data-datas='" + JSON.stringify(this.datas) + "'>"];
+        var msg = [
+            "<form class='confirm' data-datas='" +
+                JSON.stringify(this.datas) +
+                "'>",
+        ];
         msg.push("<label>" + this.datas.msg + "</label>");
-        msg.push("<a href='#' class='button btn btn-xs btn-link exit'>Annuler</a>");
-        msg.push("<a href='#' class='button btn btn-xs btn-success valid' >Oui</a>");
-        msg.push("<a href='#' class='button btn btn-xs btn-danger cancel' >Non</a>");
+        msg.push(
+            "<a href='#' class='button btn btn-xs btn-link exit'>Annuler</a>"
+        );
+        msg.push(
+            "<a href='#' class='button btn btn-xs btn-success valid' >Oui</a>"
+        );
+        msg.push(
+            "<a href='#' class='button btn btn-xs btn-danger cancel' >Non</a>"
+        );
         msg.push("</form>");
 
         return msg.join("");
     }
-
 
     popConfirmAction(datas_checked) {
         /**
@@ -33,6 +35,5 @@ class PopOverConfirm {
 
         console.log("datas_check pop");
         console.log(datas);
-
     }
 }
